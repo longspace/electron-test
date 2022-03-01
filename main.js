342eea1df94969e810c2c09131449b6bcacb8237
longspace <2928149168@qq.com>
2022-03-02 07:56:34 +0800

    热更及无边框

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const { app, BrowserWindow } = require('electron')
+
+// 热重载
+const  reloader = require("electron-reloader")
+reloader(module)
+
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        // frame:false  // 设置为true 即无黑夜的系统边框
     })
 
     win.loadFile('index.html')
+
+    // win.webContents.openDevTools();
 }
 app.whenReady().then(() => {
     createWindow()
